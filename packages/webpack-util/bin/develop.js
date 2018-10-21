e1489b3611be255551e6fee77383dad919ee8bf7
Gerald <gera2ld@163.com>
2018-10-21 23:42:56 +0800

    fix: use webpack watch mode when devServer is not enabled

--- packages/webpack-util/bin/develop.js
+++ packages/webpack-util/bin/develop.js
@@ -5,11 +5,21 @@ module.exports = develop;
 async function develop(args) {
   // Allow util to be modified when webpack.conf.js is required
   const { hasConfig, webpackPath } = require('../util/paths')(args);
+  const webpackConfig = require(webpackPath);
   const argv = [
     process.argv[0],
-    'webpack-dev-server',
   ];
+  let module;
+  if (webpackConfig.devServer) {
+    // Use webpack-dev-server, write in memory
+    argv.push('webpack-dev-server');
+    module = 'webpack-dev-server/bin/webpack-dev-server';
+  } else {
+    // Use webpack watch mode, write in file system
+    argv.push('webpack-cli', '-w');
+    module = 'webpack-cli/bin/cli';
+  }
   if (!hasConfig) argv.push('--config', webpackPath);
   process.argv = argv;
-  require('webpack-dev-server/bin/webpack-dev-server');
+  require(module);
 }
