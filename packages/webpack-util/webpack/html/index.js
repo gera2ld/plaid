2aa0a067501b0489eaa4e2bfae8a9274cdcc484d
Gerald <gera2ld@163.com>
2018-10-25 01:25:28 +0800

    feat: support injecting scripts by content

--- packages/webpack-util/webpack/html/index.js
+++ packages/webpack-util/webpack/html/index.js
@@ -1,5 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const util = {
+  escapeScript: content => content.replace(/<(\/script>)/g, '\\x3c$2'),
+};
+
 module.exports = options => config => {
   const {
     pages,
@@ -13,6 +17,7 @@ module.exports = options => config => {
     if (html) {
       options = {
         ...htmlOptions,
+        util,
         filename: `${key}.html`,
         chunks: [key],
       };
@@ -27,6 +32,12 @@ module.exports = options => config => {
     }
     if (options) {
       if (options.inlineSource) options.inject = false;
+      options.js = (options.js || [])
+      .map(item => {
+        if (typeof item === 'string') return { src: item };
+        if (item && item.content) return { content: util.escapeScript(item.content) };
+      })
+      .filter(Boolean);
       return new HtmlWebpackPlugin(options);
     }
   })
