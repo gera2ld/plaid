56f6a6df8daea03a90ade39bd7689724f2c03d6e
Gerald <gera2ld@163.com>
2018-11-10 00:32:01 +0800

    fix: add friendly-errors-webpack-plugin

--- packages/webpack-util/webpack/dev-server.js
+++ packages/webpack-util/webpack/dev-server.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const { isProd } = require('../util');
 
 module.exports = options => config => {
@@ -8,12 +9,18 @@ module.exports = options => config => {
   } = options;
   config.devServer = {
     contentBase: distDir,
+    quiet: true,
     ...devServer,
     ...config.devServer,
   };
   config.plugins = [
     ...config.plugins || [],
     !isProd && new webpack.HotModuleReplacementPlugin(),
+    !isProd && new FriendlyErrorsPlugin({
+      compilationSuccessInfo: {
+        messages: [`Your application is running here: http://localhost:${config.devServer.port || 8080}`],
+      },
+    }),
   ].filter(Boolean);
   return config;
 };
