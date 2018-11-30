04ecdb185c56f1187d42ad0c95ff39a8b74ec877
Gerald <gera2ld@163.com>
2018-11-30 22:41:23 +0800

    fix: add postcss-import plugin

--- packages/webpack-util/postcss/index.js
+++ packages/webpack-util/postcss/index.js
@@ -1,13 +1,21 @@
+const path = require('path');
+
 exports.config = {
   // Transform inline comments
   parser: require('postcss-scss'),
   plugins: [
-    require('autoprefixer'),
+    // Transform @import, resolve `#` to `$PWD/src`
+    require('postcss-import')({
+      resolve(id) {
+        if (id.startsWith('#/')) return path.resolve(`src/${id.slice(2)}`);
+        return id;
+      },
+    }),
     // Transform SCSS into CSS
     require('precss'),
     // Calculate at compile time
     require('postcss-calc'),
     // px to rem
     require('postcss-plugin-px2rem')({ rootValue: 100, minPixelValue: 2}),
-  ].filter(Boolean),
+  ],
 };
