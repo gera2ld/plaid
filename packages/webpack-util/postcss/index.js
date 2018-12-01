83044d9c841285e1b74959309b2032ea36636d1a
Gerald <gera2ld@163.com>
2018-12-02 01:27:38 +0800

    fix: add postcss-color-function

--- packages/webpack-util/postcss/index.js
+++ packages/webpack-util/postcss/index.js
@@ -13,9 +13,11 @@ exports.config = {
     }),
     // Transform SCSS into CSS
     require('precss'),
+    // Transform colors
+    require('postcss-color-function'),
     // Calculate at compile time
     require('postcss-calc'),
     // px to rem
-    require('postcss-plugin-px2rem')({ rootValue: 100, minPixelValue: 2}),
+    require('postcss-plugin-px2rem')({ rootValue: 100, minPixelValue: 2 }),
   ],
 };
