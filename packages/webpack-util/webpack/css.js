e835aa7d9c4f34383a0a85883a8619556e2270cd
Gerald <gera2ld@163.com>
2018-12-30 17:26:06 +0800

    fix: fix CSS loaders

--- packages/webpack-util/webpack/css.js
+++ packages/webpack-util/webpack/css.js
@@ -15,7 +15,7 @@ module.exports = options => config => {
     // CSS modules: src/**/*.module.css
     styleRule({
       ...styleOptions,
-      ...cssLoaders,
+      loaders: cssLoaders,
       modules: true,
     }, {
       test: /\.module\.css$/,
@@ -25,7 +25,7 @@ module.exports = options => config => {
     // normal CSS files: src/**/*.css
     styleRule({
       ...styleOptions,
-      ...cssLoaders,
+      loaders: cssLoaders,
     }, {
       exclude: [/\.module\.css$/, nodeModules],
     }),
