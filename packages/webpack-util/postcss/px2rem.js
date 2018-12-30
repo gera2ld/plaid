7658d255a205082a1dd215e3093485a49d153ec4
Gerald <gera2ld@163.com>
2018-12-31 00:20:49 +0800

    fix: add px2rem as a configurator

--- packages/webpack-util/postcss/px2rem.js
+++ packages/webpack-util/postcss/px2rem.js
@@ -1,7 +1,8 @@
-module.exports = config => {
+module.exports = () => config => {
   config.plugins = [
     ...config.plugins || [],
     // px to rem
     require('postcss-plugin-px2rem')({ rootValue: 100, minPixelValue: 2 }),
   ];
+  return config;
 };
