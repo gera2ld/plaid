b6275000af06f93e604879cece8e6c00831d68c2
Gerald <gera2ld@163.com>
2019-02-20 13:09:14 +0800

    fix: disable px2rem by default

--- packages/plaid/config/postcssrc.js
+++ packages/plaid/config/postcssrc.js
@@ -1,5 +1,4 @@
 const { combineConfigSync } = require('../util/helpers');
 const precss = require('../postcss/precss');
-const px2rem = require('../postcss/px2rem');
 
-module.exports = combineConfigSync({}, [precss, px2rem]);
+module.exports = combineConfigSync({}, [precss]);
