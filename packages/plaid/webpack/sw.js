48a3dd59a3a4c6140ec96daea880eb4d4b84d1e8
Gerald <gera2ld@163.com>
2019-03-19 16:53:26 +0800

    fix: load workbox from jsdelivr by default

--- packages/plaid/webpack/sw.js
+++ packages/plaid/webpack/sw.js
@@ -7,26 +7,29 @@ module.exports = async (config, options) => {
     swOptions,
   } = options;
   if (!swOptions) return;
-  const { runtimeCaching, ...rest } = swOptions;
+  const { importWorkboxFrom, runtimeCaching, ...rest } = swOptions;
 
   await fs.mkdir(tmpDir, { recursive: true });
-  const runtimeCachingCode = (runtimeCaching || [])
-  .map(({ urlPattern, handler }) => `workbox.routing.registerRoute(${urlPattern}, new workbox.strategies.${handler}(), 'GET');`)
-  .join('\n');
-  const swCode = `\
+  const swCode = [
+    importWorkboxFrom == null && `\
 importScript('https://cdn.jsdelivr.net/npm/workbox-sw@4.1.1/build/workbox-sw.min.js');
 workbox.setConfig({
   modulePathPrefix: 'https://cdn.jsdelivr.net/npm/workbox-sw@4.1.1/build',
-});
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-${runtimeCachingCode}`;
+});`,
+
+    'workbox.precaching.precacheAndRoute(self.__precacheManifest, {});',
+
+    // runtimeCaching
+    ...(runtimeCaching || [])
+    .map(({ urlPattern, handler }) => `workbox.routing.registerRoute(${urlPattern}, new workbox.strategies.${handler}(), 'GET');`),
+  ].filter(Boolean).join('\n');
   await fs.writeFile(`${tmpDir}/sw.js`, swCode, 'utf8');
 
   config.plugins = [
     ...config.plugins || [],
     new InjectManifest({
       swSrc: `${tmpDir}/sw.js`,
-      importWorkboxFrom: 'disabled',
+      importWorkboxFrom: importWorkboxFrom == null ? 'disabled' : importWorkboxFrom,
       ...rest,
     }),
   ];
