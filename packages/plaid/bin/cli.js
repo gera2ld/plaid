9ac4bdcbc0ee62262d5ef6d9740462abc67bc799
Gerald <gera2ld@163.com>
2019-04-14 03:33:46 +0800

    fix: throw on unknown command

--- packages/plaid/bin/cli.js
+++ packages/plaid/bin/cli.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 const { program } = require('./program');
-const { requireSilent } = require('../util');
+const { requireSilent, exitError } = require('../util');
 
 requireSilent('@gera2ld/plaid-webpack/bin');
+
+program
+.command('*')
+.action((name) => {
+  exitError(1, `Unknown command: ${name}`);
+});
 
 program.parse(process.argv);
