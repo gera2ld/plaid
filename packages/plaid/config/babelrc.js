7495adc04a5ae94114a4512f9ec7df5dc0c4e40f
Gerald <gera2ld@163.com>
2019-04-14 22:13:15 +0800

    fix: extract base babel config

--- packages/plaid/config/babelrc.js
+++ packages/plaid/config/babelrc.js
@@ -1,6 +1,7 @@
 const { defaultOptions } = require('../util');
 
 module.exports = {
+  extends: require.resolve('./babelrc-base'),
   presets: [
     ['@babel/preset-env', {
       ...process.env.BABEL_ENV !== 'test' && {
@@ -12,24 +13,6 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-transform-runtime',
-
-    // stage-1
-    '@babel/plugin-proposal-export-namespace-from',
-    '@babel/plugin-proposal-export-default-from',
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' }],
-
-    // stage-2
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    '@babel/plugin-proposal-function-sent',
-    '@babel/plugin-proposal-numeric-separator',
-    '@babel/plugin-proposal-throw-expressions',
-
-    // stage-3
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-syntax-import-meta',
-    // Use loose mode: facebook/create-react-app#4263
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-json-strings',
 
     ['module-resolver', {
       alias: defaultOptions.alias,
