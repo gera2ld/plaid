f82ca029dbd5e668a7373c4337c0b9a892e71a1d
Gerald <gera2ld@live.com>
2020-12-10 12:55:17 +0800

    fix: drop vue-style-loader because it does not support latest css-loader
    Ref: https://github.com/vuejs/vue-style-loader/issues/46

--- packages/plaid-webpack-vue/util/index.js
+++ packages/plaid-webpack-vue/util/index.js
@@ -1,6 +1,5 @@
 const { defaultOptions } = require('@gera2ld/plaid/util');
 
-defaultOptions.styleOptions.fallback = require.resolve('vue-style-loader');
 defaultOptions.vueOptions = {
   compilerOptions: {
     whitespace: 'condense',
