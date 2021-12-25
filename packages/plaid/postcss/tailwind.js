28a14464d3b530deb5fd41b4e0145ab32f37e8ef
Gerald <gera2ld@live.com>
2021-12-26 00:40:08 +0800

    fix: update postcss config

--- packages/plaid/postcss/tailwind.js
+++ packages/plaid/postcss/tailwind.js
@@ -3,6 +3,7 @@ const { defaultOptions } = require('../util');
 module.exports = config => {
   config.plugins = [
     ...config.plugins || [],
+    require('tailwindcss/nesting'),
     require('tailwindcss')(defaultOptions.tailwindcss),
   ];
   return config;
